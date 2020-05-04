a9a8671fbb6295ac41fe076dda5bb71f70855998
Bart Spaans <bart.spaans@gmail.com>
2020-05-04 14:45:22 +0100

    'Finished' implementing new SquareControl, but is has bugs. Added failing tests, but going to sleep. Cya

--- square_control_test.go
+++ square_control_test.go
@@ -70,3 +70,47 @@ func Test_SquareControl_own_king_is_not_ignored(t *testing.T) {
 		expectQueenFromAt(t, unit, E6, pos)
 	}
 }
+
+func Test_SquareControl_ApplyMove(t *testing.T) {
+
+	board := NewBoard()
+	board[E6] = WhiteQueen
+	board[E3] = WhiteKing
+	orig := NewSquareControl()
+
+	orig.addPiece(WhiteQueen, E6, board)
+	orig.addPiece(WhiteKing, E3, board)
+
+	board.ApplyMove(E3, D3)
+	unit := orig.ApplyMove(NewMove(E3, D3), WhiteKing, NoPiece, board, NoPosition)
+
+	positions := E6.GetPieceMoves(WhiteQueen)
+	for _, pos := range positions {
+		if unit[int(White)*64+int(pos)].Count() == 0 {
+			t.Errorf("Expecting an attack on %s", pos)
+		}
+		expectQueenFromAt(t, unit, E6, pos)
+	}
+	board.ApplyMove(D3, E3)
+	unit = unit.ApplyMove(NewMove(D3, E3), WhiteKing, NoPiece, board, NoPosition)
+	if unit[int(White)*64+int(C3)].Count() != 0 {
+		t.Errorf("Expecting old king attacks to be removed")
+	}
+	for _, pos := range positions {
+		if orig[int(White)*64+int(pos)].Count() != unit[int(White)*64+int(pos)].Count() {
+			t.Fatalf("expecting same amount of attacks again.")
+		}
+		if pos == E1 || pos == E2 {
+			if unit[int(White)*64+int(pos)].Count() > 1 {
+				t.Errorf("Expecting no attacks on %s, got %v", pos, unit[int(White)*64+int(pos)].ToPositions())
+			}
+			continue
+		} else if unit[int(White)*64+int(pos)].Count() == 0 {
+			t.Errorf("Expecting an attack on %s", pos)
+		} else if unit[int(White)*64+int(pos)].Count() != 1 {
+			t.Errorf("Expecting white queen in piece vector")
+		} else if unit[int(White)*64+int(pos)].ToPositions()[0] != E6 {
+			t.Errorf("Expecting e6 got %s", unit[int(White)*64+int(pos)].ToPositions()[0])
+		}
+	}
+}
